22e048d018426bc212907d456b52c7f26228adfb
JoshiSankar-4021 <joshisankar5051@gmail.com>
2025-09-22 12:09:33 +0530

    Profile is working in progresss

--- src/Profile/Profile.jsx
+++ src/Profile/Profile.jsx
@@ -1,158 +1,158 @@
-import { NavLink } from 'react-router-dom';
-import {} from 'react-icons';
-import { MdMail,MdLock,MdLocationOn,MdPhoneAndroid} from 'react-icons/md';
-import { FaGraduationCap,FaChevronDown,FaRegUser } from 'react-icons/fa';
-import { useState } from 'react';
-import { IoMdEye, IoMdEyeOff } from 'react-icons/io';
+    import { useEffect, useState } from "react";
+    import { MdMail, MdLock, MdLocationOn, MdPhoneAndroid } from "react-icons/md";
+    import { FaGraduationCap, FaChevronDown, FaRegUser } from "react-icons/fa";
+    import { IoMdEye, IoMdEyeOff } from "react-icons/io";
 
+    function Register() {
+    const [showPassword, setShowPassword] = useState(false);
+    const [profiledata, setProfiledata] = useState({
+        firstname: "",
+        lastname: "",
+        email: "",
+        password: "",
+        retypepassword: "",
+        address: "",
+        gender: "",
+        phone: "",
+        education: ""
+    });
 
-function Register(){
-        const [showPassword, setShowPassword] = useState(false);
-    
-    return(
+    // Fetch profile from backend
+    useEffect(() => {
+        const userId = sessionStorage.getItem("userid");
+        if (userId) {
+        fetch(`${process.env.REACT_APP_BACKEND_URL}/api/User?action=getprofile&userid=${userId}`)
+            .then((res) => res.json())
+            .then((data) => {
+            if (data?.data) {
+                // Check if backend returns array or single object
+                const profile = Array.isArray(data.data) ? data.data[0] : data.data;
+                setProfiledata(profile);
+            }
+            })
+            .catch((err) => console.error("Error fetching profile:", err));
+        }
+    }, []);
+
+    // Update state when user types
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setProfiledata((prev) => ({
+        ...prev,
+        [name]: value
+        }));
+    };
+
+    // Submit updated profile
+    const handleUpdate = async (e) => {
+        e.preventDefault();
+        const userId = sessionStorage.getItem("userid");
+
+        try {
+        console.log("Submitting profile data:", profiledata);
+
+        const res = await fetch(`${process.env.REACT_APP_BACKEND_URL}/api/User?action=updateprofile&userid=${userId}`, {
+            method: "PUT",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(profiledata) // send full profile or only changed fields
+        });
+
+        const data = await res.json();
+        console.log("Update response:", data);
+        alert("Profile updated successfully!");
+        } catch (err) {
+        console.error("Update error:", err);
+        alert("Failed to update profile");
+        }
+    };
+
+    const togglePasswordVisibility = () => setShowPassword(!showPassword);
+
+    return (
         <div className="register-container">
-            <h1 className='register-title'>Profile</h1>
-            <div className="registerform-container">
-                <form>
-                    <label className="register-labelStyle">First Name</label>
-                    <div className='input-wrapper'>
-                    <FaRegUser  className='input-icon'/>
-                    <input type="text" className="register-inputStyle"/>
-                    </div>
-                
-                    <label className="register-labelStyle">Last Name</label>
-                    <div className='input-wrapper'>
-                    <FaRegUser className='input-icon'/>
-                    <input type="text" className="register-inputStyle"/>
-                    </div>
-                    
-                    <label className="register-labelStyle">EMAIL</label>
-                    <div className='input-wrapper'>
-                    <MdMail className='input-icon'/>
-                    <input type="text" className="register-inputStyle"/>
-                    </div>
-
-                    <label className="register-labelStyle">Password</label>
-                    <div className='input-wrapper'>
-                    <MdLock className='input-icon'/>
-                    <input type={showPassword?'text':'password'} className="register-inputStyle"/>
-                    {showPassword?(
-                        <IoMdEye className='inputicon-right' onClick={togglePasswordVisibility}/>):(
-                        <IoMdEyeOff className='inputicon-right' onClick={togglePasswordVisibility}/>)
-                    }
-                    </div>
-
-                    <label className="register-labelStyle">Re-TypePassword</label>
-                    <div className='input-wrapper'>
-                    <MdLock className='input-icon'/>
-                    <input type={showPassword?'text':'password'} className="register-inputStyle"/>
-                    {showPassword?(
-                        <IoMdEye className='inputicon-right' onClick={togglePasswordVisibility}/>):(
-                        <IoMdEyeOff className='inputicon-right' onClick={togglePasswordVisibility}/>)
-                    }
-                    </div>
-
-                    <label className="register-labelStyle">Address</label>
-                    <div className='input-wrapper'>
-                    <MdLocationOn className='input-icon'/>
-                    <textarea type="text" className="register-inputStyle"/>
-                    </div>
-                    
-
-                    <label className="register-labelStyle">Eduction</label>
-                    <div className="select-wrapper">
-                    <FaGraduationCap className="select-icon" />
-                    <select className="register-inputStyle with-left-icon">
-                        <option>Select</option>
-                        <option>B-Tech</option>
-                        <option>M-Tech</option>
-                        <option>B.SC</option>
-                        <option>BCA</option>
-                        <option>MCA</option>
-                    </select>
-                    <FaChevronDown className="select-arrow"/>
-                    </div>
-
-                    <label className='register-labelStyle'>Phone</label>
-                    <div className='input-wrapper'>
-                    <MdPhoneAndroid className='input-icon'/>
-                    <input className='register-inputStyle'/>
-                    </div>
-
-                    <label  className='register-labelStyle'>Gender</label>
-                    <label className='register-label-radio'>Male</label>
-                    <input  type="radio" name='gender' className='register-radio-input'/>
-
-                    <label className='register-label-radio'>Fe-Male</label>
-                    <input type="radio" name='gender' className='register-radio-input'/>
-
-                    <label className="register-labelStyle">Skills</label>
-                    <table>
-                        <tr>
-                            <td>
-                            <label className='register-checkboxName'>Java</label>
-                            </td>
-                            <td>
-                            <input type="checkbox" className='register-checkbox-input'/>
-                            </td>
-                            <td>
-                            <label className='register-checkboxName'>Python</label>
-                            </td>
-                            <td>
-                            <input type="checkbox" className='register-checkbox-input'/>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                            <label className='register-checkboxName'>Sql</label>
-                            </td>
-                            <td>
-                            <input type="checkbox" className='register-checkbox-input'/>
-                            </td>
-                            <td>
-                            <label className='register-checkboxName'>React</label>
-                            </td>
-                            <td>
-                                <input type="checkbox" className='register-checkbox-input'/>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <label className='register-checkboxName'>Node</label>
-                            </td>
-                            <td>
-                                <input type="checkbox" className='register-checkbox-input'/>
-                            </td>
-                            <td>
-                                <label className='register-checkboxName'>Java Script</label>
-                            </td>
-                            <td>
-                                <input type="checkbox" className='register-checkbox-input'/>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                            <label className='register-checkboxName'>Express</label>
-                            </td>
-                            <td>
-                            <input type="checkbox" className='register-checkbox-input'/>
-                            </td>
-                            <td>
-                            <label className='register-checkboxName'>Html&Css</label>
-                            </td>
-                            <td>
-                            <input type="checkbox" className='register-checkbox-input'/>
-                            </td>
-                        </tr>
-                    </table>
-
-                    <button className='register-buttonstyle'>Update</button>
-                </form>
+        <h1 className="register-title">Profile</h1>
+        <div className="registerform-container">
+            <form onSubmit={handleUpdate}>
+            {/* First Name */}
+            <label className="register-labelStyle">First Name</label>
+            <div className="input-wrapper">
+                <FaRegUser className="input-icon" />
+                <input type="text" name="firstname" value={profiledata.firstname} onChange={handleChange} className="register-inputStyle" />
             </div>
+
+            {/* Last Name */}
+            <label className="register-labelStyle">Last Name</label>
+            <div className="input-wrapper">
+                <FaRegUser className="input-icon" />
+                <input type="text" name="lastname" value={profiledata.lastname} onChange={handleChange} className="register-inputStyle" />
+            </div>
+
+            {/* Email */}
+            <label className="register-labelStyle">Email</label>
+            <div className="input-wrapper">
+                <MdMail className="input-icon" />
+                <input type="text" name="email" value={profiledata.email} onChange={handleChange} className="register-inputStyle" />
+            </div>
+
+            {/* Password */}
+            <label className="register-labelStyle">Password</label>
+            <div className="input-wrapper">
+                <MdLock className="input-icon" />
+                <input type={showPassword ? "text" : "password"} name="password" value={profiledata.password} onChange={handleChange} className="register-inputStyle" />
+                {showPassword ? (
+                <IoMdEye className="inputicon-right" onClick={togglePasswordVisibility} />
+                ) : (
+                <IoMdEyeOff className="inputicon-right" onClick={togglePasswordVisibility} />
+                )}
+            </div>
+
+            {/* Re-Type Password */}
+            <label className="register-labelStyle">Re-Type Password</label>
+            <div className="input-wrapper">
+                <MdLock className="input-icon" />
+                <input type={showPassword ? "text" : "password"} name="retypepassword" value={profiledata.retypepassword} onChange={handleChange} className="register-inputStyle" />
+            </div>
+
+            {/* Address */}
+            <label className="register-labelStyle">Address</label>
+            <div className="input-wrapper">
+                <MdLocationOn className="input-icon" />
+                <textarea name="address" value={profiledata.address} onChange={handleChange} className="register-inputStyle" />
+            </div>
+
+            {/* Education */}
+            <label className="register-labelStyle">Education</label>
+            <div className="select-wrapper">
+                <FaGraduationCap className="select-icon" />
+                <select name="education" value={profiledata.education} onChange={handleChange} className="register-inputStyle with-left-icon">
+                <option value="">Select Education</option>
+                <option value="B.Tech">B.Tech</option>
+                <option value="M.Tech">M.Tech</option>
+                <option value="B.Sc">B.Sc</option>
+                <option value="M.Sc">M.Sc</option>
+                </select>
+                <FaChevronDown className="select-arrow" />
+            </div>
+
+            {/* Phone */}
+            <label className="register-labelStyle">Phone</label>
+            <div className="input-wrapper">
+                <MdPhoneAndroid className="input-icon" />
+                <input type="text" name="phone" value={profiledata.phone} onChange={handleChange} className="register-inputStyle" />
+            </div>
+
+            {/* Gender */}
+            <label className="register-labelStyle">Gender</label>
+            <label className="register-label-radio">Male</label>
+            <input type="radio" name="gender" value="Male" checked={profiledata.gender === "Male"} onChange={handleChange} className="register-radio-input" />
+            <label className="register-label-radio">Female</label>
+            <input type="radio" name="gender" value="Female" checked={profiledata.gender === "Female"} onChange={handleChange} className="register-radio-input" />
+
+            <button type="submit" className="register-buttonstyle">Update</button>
+            </form>
+        </div>
         </div>
     );
-    function togglePasswordVisibility() {
-    setShowPassword(!showPassword);
-}
-}
-export default Register;
+    }
+
+    export default Register;
